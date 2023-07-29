e679267dd7a63aa11445493c7ccfe7d042a8d108
gayathris99 <gayathri11499@gmail.com>
2023-07-29 10:46:51 +0530

    Want to make appointment

---
+++ src/components/HomePageComponents/MakeAppointment.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container"></div>
+  <div class="bg-bar" :class="isDesktop ? 'q-px-xl q-py-lg' : 'q-pa-md'">
+    <div class="row justify-between items-center q-gutter-md make-appt">
+      <div class="text-white font-radley title text-bold">Want to make an appointment now?</div>
+      <q-btn
+      style="background: #D05757; color: white"
+      label="Make an appointment"
+      rounded
+      no-caps
+      class="font-overlock"
+      :size="isDesktop ? 'lg' : 'md'"
+      />
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'MakeAppointment',
+  computed: {
+  isDesktop () {
+    return this.$q.screen.gt.sm
+  }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 500px;
+  background-image: url('~assets/imagebg1.svg');
+  background-size: cover;
+  background-position: center;
+  @media only screen and (max-width: $breakpoint-sm-max) {
+    height: 350px;
+  }
+  @media only screen and (max-width: $breakpoint-xs-max) {
+    height: 210px;
+  }
+}
+
+.bg-bar {
+  background-color: #21222A;
+}
+.title {
+  font-size: 24px;
+  @media only screen and (max-width: $breakpoint-sm-max) {
+    font-size: 20px;
+   }
+}
+.make-appt {
+   @media only screen and (max-width: 516px) {
+    justify-content: center;
+   }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,6 +5,7 @@
     <hero-navbar/>
     <why-choseus/>
     <our-doctors/>
+    <make-appointment/>
   </q-page>
 </template>
 
@@ -14,9 +15,10 @@ import HeroSection from 'src/components/HomePageComponents/HeroSection.vue'
 import HeroNavbar from 'src/components/HomePageComponents/HeroNavbar.vue'
 import WhyChoseus from 'src/components/HomePageComponents/WhyChoseus.vue'
 import OurDoctors from 'src/components/HomePageComponents/OurDoctors.vue'
+import MakeAppointment from 'src/components/HomePageComponents/MakeAppointment.vue'
 export default {
   name: 'IndexPage',
-  components: { HeaderComponent, HeroSection, HeroNavbar, WhyChoseus, OurDoctors }
+  components: { HeaderComponent, HeroSection, HeroNavbar, WhyChoseus, OurDoctors, MakeAppointment }
 }
 </script>
 
