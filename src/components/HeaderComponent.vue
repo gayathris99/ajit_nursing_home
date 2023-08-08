e0cd9078238a487108b09ab61fcae4af10b0fa54
gayathris99 <gayathri11499@gmail.com>
2023-08-08 20:19:24 +0530

    About Layout

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,11 +3,11 @@
     <div class="row justify-between align-center">
       <div class="row items-center q-gutter-sm cursor-pointer">
         <q-icon name="menu" size="md" v-if="!isDesktop" class="text-primary cursor-pointer q-pr-sm" @click="openDrawer"/>
-        <img src="~assets/Logo128.png" alt="Ajit nursing home logo" width="45">
-        <div :class="isDesktop ? 'fs-24' : 'fs-22'">Ajit Nursing Home</div>
+        <img src="~assets/Logo128.png" alt="Ajit nursing home logo" width="45" @click="goTo('home')">
+        <div :class="isDesktop ? 'fs-24' : 'fs-22'" @click="goTo('home')">Ajit Nursing Home</div>
       </div>
       <div class="row items-center q-gutter-md fs-16 text-bold" v-if="isDesktop">
-        <div class="cursor-pointer">About Us</div>
+        <div class="cursor-pointer" @click="goTo('about-us')">About Us</div>
         <div class="cursor-pointer" @click="comingSoon">Pregnancy</div>
         <div class="cursor-pointer" @click="comingSoon">Getting Pregnant</div>
         <div class="cursor-pointer" @click="comingSoon">Tools</div>
@@ -18,12 +18,12 @@
     </div>
     <q-drawer v-if="!isDesktop"
     v-model="openMenu">
-      <div class="row items-center q-gutter-sm bg-header q-pa-md">
+      <div class="row items-center q-gutter-sm bg-header q-pa-md" @click="goTo('home')">
         <img src="~assets/Logo128.png" alt="Ajit nursing home logo" width="50">
         <div class="fs-20">Ajit Nursing Home</div>
       </div>
       <div class="column q-gutter-md text-bold q-mt-sm q-pa-md">
-        <div class="cursor-pointer">About Us</div>
+        <div class="cursor-pointer" @click="goTo('about-us')">About Us</div>
         <div class="cursor-pointer" @click="comingSoon">Pregnancy</div>
         <div class="cursor-pointer" @click="comingSoon">Getting Pregnant</div>
         <div class="cursor-pointer" @click="comingSoon">Tools</div>
@@ -53,6 +53,11 @@ export default {
         color: "green",
         position: "top"
       });
+    },
+    goTo (route) {
+      this.$router.push({
+        name: route
+      })
     }
   },
   computed: {
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', name: 'home', component: () => import('pages/IndexPage.vue') },
+      { path: 'about-us', name: 'about-us', component: () => import('pages/AboutUs.vue') }
     ]
   },
 
