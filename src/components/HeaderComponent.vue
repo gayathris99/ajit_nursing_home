c19ae6d77a16398422285ad1b00ee25673bebc9a
gayathris99 <gayathri11499@gmail.com>
2023-08-16 11:25:56 +0530

    Header Redesign

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-header class="bg-header text-primary font-petrona q-py-sm" :class="isDesktop ? 'q-px-xl': 'q-px-sm'">
+  <q-header class="bg-header text-primary font-quicksand q-py-sm" :class="isDesktop ? 'q-px-xl': 'q-px-sm'">
     <div class="row justify-between align-center">
       <div class="row items-center q-gutter-sm cursor-pointer">
         <q-icon name="menu" size="md" v-if="!isDesktop" class="text-primary cursor-pointer q-pr-sm" @click="openDrawer"/>
         <img src="~assets/Logo128.png" alt="Ajit nursing home logo" width="45" @click="goTo('home')">
-        <div :class="isDesktop ? 'fs-24' : 'fs-22'" @click="goTo('home')">Ajit Nursing Home</div>
+        <div class="fs-16 fw-600" @click="goTo('home')">Ajit Nursing Home</div>
       </div>
-      <div class="row items-center q-gutter-md fs-16 text-bold" v-if="isDesktop">
+      <div class="row items-center q-gutter-md fs-16 fw-600" v-if="isDesktop">
         <div class="cursor-pointer" @click="goTo('about-us')">About Us</div>
         <div class="cursor-pointer" @click="comingSoon">Pregnancy</div>
         <div class="cursor-pointer" @click="comingSoon">Getting Pregnant</div>
@@ -20,9 +20,9 @@
     v-model="openMenu">
       <div class="row items-center q-gutter-sm bg-header q-pa-md" @click="goTo('home')">
         <img src="~assets/Logo128.png" alt="Ajit nursing home logo" width="50">
-        <div class="fs-20">Ajit Nursing Home</div>
+        <div class="fs-20 fw-600">Ajit Nursing Home</div>
       </div>
-      <div class="column q-gutter-md text-bold q-mt-sm q-pa-md">
+      <div class="column q-gutter-md fw-600 q-mt-sm q-pa-md">
         <div class="cursor-pointer" @click="goTo('about-us')">About Us</div>
         <div class="cursor-pointer" @click="comingSoon">Pregnancy</div>
         <div class="cursor-pointer" @click="comingSoon">Getting Pregnant</div>
