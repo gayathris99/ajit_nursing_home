7bcfb2b4c6eeb53f9f7f49621d92aa2e293c8782
gayathris99 <gayathri11499@gmail.com>
2023-10-01 17:56:07 +0530

    Family infor

--- src/components/FamilySettingComponents/FamilyInfo.vue
+++ src/components/FamilySettingComponents/FamilyInfo.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="q-mt-lg">
+  <div class="q-mt-lg font-montserrat">
     <div class="font-domine subtitle fw-500 text-primary">My Family</div>
+    <q-checkbox v-model="isTryingToConceive" class="fs-16 q-mt-md fw-500 font-montserrat text-primary" label="Trying to conceive" color="secondary" @click="() => tryingToConceivePopup = true"/>
+    <div class="q-mt-md fw-700 fs-18 text-primary">My pregnancy</div>
+    <div class="border-bottom q-my-lg"></div>
+    <div class="row items-center justify-between">
+      <div>
+        <div class="fw-700 fs-16 text-primary">My pregnancy</div>
+        <div class="row items-center justify-between q-gutter-x-sm q-mt-sm">
+          <q-icon name="event" size="sm" color="grey"></q-icon>
+          <div class="text-grey fw-500 fs-16">April 11 2024</div>
+        </div>
+      </div>
+      <div>
+        <q-icon name="edit" color="secondary" class="cursor-pointer" size="sm"></q-icon>
+      </div>
+    </div>
+    <div class="border-bottom q-my-lg"></div>
+    <div class="row items-center q-gutter-x-sm cursor-pointer">
+      <q-icon name="add" size="sm" color="secondary"></q-icon>
+      <div class="fs-16 fw-700 text-secondary">Add Pregnancy</div>
+    </div>
+    <div class="border-bottom q-my-lg"></div>
+    <div class="q-mt-xl fw-700 fs-18 text-primary">My children</div>
+    <div class="border-bottom q-my-lg"></div>
+    <div class="row items-center q-gutter-x-sm cursor-pointer">
+      <q-icon name="add" size="sm" color="secondary"></q-icon>
+      <div class="fs-16 fw-700 text-secondary">Add a child</div>
+    </div>
+    <div class="border-bottom q-mt-lg q-mb-xl"></div>
   </div>
+  <q-dialog v-model="tryingToConceivePopup">
+    <trying-to-conceive-popup/>
+  </q-dialog>
 </template>
+
+<script>
+import TryingToConceivePopup from './TryingToConceivePopup.vue'
+export default {
+  name: 'FamilyInfo',
+  components: { TryingToConceivePopup },
+  data () {
+    return {
+      isTryingToConceive: false,
+      tryingToConceivePopup: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.border-bottom {
+  border-bottom: 1px solid grey
+}
+</style>
---
+++ src/components/FamilySettingComponents/TryingToConceivePopup.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-card class="no-padding">
+    <q-card-section>
+      <div class="row items-center justify-end cursor-pointer q-pt-sm q-pr-sm" v-close-popup>
+        <img src="~assets/closeIconSecondary.svg" width="24" alt="">
+      </div>
+      <div class="q-px-md q-pb-md q-pt-sm font-montserrat">
+        <div class="fw-700 text-primary fs-18 q-mb-lg">Trying to conceive</div>
+        <div class="q-mt-md fw-700 fs-16 text-primary">Email Subscriptions</div>
+        <div class="border-bottom q-my-lg"></div>
+         <q-checkbox v-model="isTryingToConceive" class="fs-16 fw-500 font-montserrat text-primary" label="My newsletter with personalized baby development info and expert tips." color="secondary"/>
+        <div class="q-mt-lg fs-12 fw-700 text-grey">
+          You can unsubscribe at any time. By continuing, you agree to our <span class="cursor-pointer text-underline" @click="goTo('terms')">Terms of Use</span> and <span class="cursor-pointer text-underline"  @click="goTo('privacy')">Privacy Policy</span>.
+        </div>
+        <div class="flex flex-center q-mt-lg">
+          <q-btn
+            class="q-my-sm save-btn font-montserrat"
+            label="SAVE"
+            color="secondary"
+            size="sm"
+            ></q-btn>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+
+<script>
+export default {
+  name: 'TryingToConceivePopup',
+  data () {
+    return {
+      isTryingToConceive: false
+    }
+  },
+  methods: {
+    goTo (name) {
+      this.$router.push({
+        name
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.border-bottom {
+  border-bottom: 1px solid grey
+}
+.q-card {
+  max-width: 450px;
+}
+.save-btn {
+  width: 120px;
+  height: 50px;
+  font-weight: bolder;
+  padding: 0px 12px;
+  :deep(.q-btn__content) {
+    font-size: 18px;
+  }
+}
+</style>
