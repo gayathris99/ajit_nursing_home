71c0ae762377b917c4b9dd597f8fd037fdfe0219
gayathris99 <gayathri11499@gmail.com>
2023-10-25 12:48:31 +0530

    Trying to conceive popup - add whatsapp

--- src/components/FamilySettingComponents/TryingToConceivePopup.vue
+++ src/components/FamilySettingComponents/TryingToConceivePopup.vue
@@ -8,7 +8,10 @@
         <div class="fw-700 text-primary fs-18 q-mb-lg">Trying to conceive</div>
         <div class="q-mt-md fw-700 fs-16 text-primary">Email Subscriptions</div>
         <div class="border-bottom q-my-lg"></div>
-         <q-checkbox v-model="isTryingToConceive" class="fs-16 fw-500 font-montserrat text-primary" label="My newsletter with personalized baby development info and expert tips." color="secondary"/>
+         <q-checkbox v-model="isTryingToConceiveEmail" class="fs-16 fw-500 font-montserrat text-primary" label="My newsletter with personalized baby development info and expert tips." color="secondary"/>
+        <div class="fw-700 fs-16 text-primary q-mt-xl">Whatsapp Subscriptions</div>
+        <div class="border-bottom q-my-lg"></div>
+         <q-checkbox v-model="isTryingToConceiveWhatsapp" class="fs-16 fw-500 font-montserrat text-primary" label="My newsletter with personalized baby development info and expert tips." color="secondary"/>
         <div class="q-mt-lg fs-12 fw-700 text-grey">
           You can unsubscribe at any time. By continuing, you agree to our <span class="cursor-pointer text-underline" @click="goTo('terms')">Terms of Use</span> and <span class="cursor-pointer text-underline"  @click="goTo('privacy')">Privacy Policy</span>.
         </div>
@@ -31,7 +34,8 @@ export default {
   name: 'TryingToConceivePopup',
   data () {
     return {
-      isTryingToConceive: false
+      isTryingToConceiveEmail: false,
+      isTryingToConceiveWhatsapp: false
     }
   },
   methods: {
@@ -59,4 +63,7 @@ export default {
     font-size: 18px;
   }
 }
+  :deep(.q-checkbox__label) {
+    margin-left: 8px;
+  }
 </style>
